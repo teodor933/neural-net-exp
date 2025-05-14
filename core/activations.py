Normalised temporary inputs to avoid overflow errors. Added type hints. ### SGD with Momentum currently broken, must be fixed ###

core/activations.py
@@ -1,36 +1,35 @@
 from abc import abstractmethod, ABC
 import numpy as np
-from PIL.ImageOps import scale
 
 from core.initialisers import HeNormal, Normal
 
 class Activation(ABC):
     @abstractmethod
-    def execute(self, x):
+    def execute(self, x: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
     @abstractmethod
-    def derivative(self, x):
+    def derivative(self, x: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
 class Linear(Activation): # Regression
     def __init__(self):
         self.weight_initialiser = Normal(scale=0.01)
 
-    def execute(self, x):
+    def execute(self, x: np.ndarray) -> np.ndarray:
         return x
 
-    def derivative(self, x):
+    def derivative(self, x: np.ndarray) -> np.ndarray:
         return np.ones_like(x)
 
 class ReLU(Activation):
     def __init__(self):
         self.weight_initialiser = HeNormal()
 
-    def execute(self, x):
+    def execute(self, x: np.ndarray) -> np.ndarray:
         return np.maximum(0, x)
 
-    def derivative(self, x):
+    def derivative(self, x: np.ndarray) -> np.ndarray:
         return np.where(x > 0, 1, 0)
 
 class LeakyReLU(Activation):
@@ -38,20 +37,20 @@ class LeakyReLU(Activation):
         self.alpha = alpha
         self.weight_initialiser = HeNormal()
 
-    def execute(self, x):
+    def execute(self, x: np.ndarray) -> np.ndarray:
         return np.where(x > 0, x, self.alpha * x)
 
-    def derivative(self, x):
+    def derivative(self, x: np.ndarray) -> np.ndarray:
         return np.where(x > 0, 1, self.alpha)
 
 class Tanh(Activation):
     def __init__(self):
         self.weight_initialiser = Normal(scale=0.01)
 
-    def execute(self, x):
+    def execute(self, x: np.ndarray) -> np.ndarray:
         return np.tanh(x)
 
-    def derivative(self, x):
+    def derivative(self, x: np.ndarray) -> np.ndarray:
         return 1 - np.tanh(x) ** 2
 
 
@@ -59,10 +58,10 @@ class Softmax(Activation): # Classification
     def __init__(self):
         self.weight_initialiser = Normal(scale=0.01)
 
-    def execute(self, x):
+    def execute(self, x: np.ndarray) -> np.ndarray:
         exp_x = np.exp(x - np.max(x))  # Stability trick to prevent overflow
         return exp_x / exp_x.sum(axis=0, keepdims=True)
 
-    def derivative(self, x):
+    def derivative(self, x: np.ndarray) -> np.ndarray:
         s = Softmax.execute(self, x)
         return np.diagflat(s) - np.outer(s, s)

core/initialisers.py
@@ -4,11 +4,11 @@ from abc import ABC, abstractmethod
 
 class Initialiser(ABC):
     @abstractmethod
-    def __call__(self, input_size, output_size):
+    def __call__(self, input_size: int, output_size: int) -> np.ndarray:
         raise NotImplementedError
 
 class HeNormal(Initialiser):
-    def __call__(self, input_size, output_size):
+    def __call__(self, input_size: int, output_size: int) -> np.ndarray:
         scale = np.sqrt(2.0 / input_size)
         return np.random.randn(output_size, input_size) * scale
 
@@ -16,5 +16,5 @@ class Normal(Initialiser):
     def __init__(self, scale=0.01):
         self.scale = scale
 
-    def __call__(self, input_size, output_size):
+    def __call__(self, input_size: int, output_size: int) -> np.ndarray:
         return np.random.randn(output_size, input_size) * self.scale

core/layers.py
@@ -1,35 +1,44 @@
 import numpy as np
 
-from core.activations import LeakyReLU
+from core.activations import LeakyReLU, Activation
+from core.initialisers import Initialiser
+from core.optimisers import Optimiser
+
+from typing import Tuple, Optional
 
 
 class Layer:
-    def __init__(self, input_size, output_size, activation, weight_initialiser=None):
+    def __init__(self,
+                 input_size: int,
+                 output_size: int,
+                 activation: Activation,
+                 weight_initialiser: Initialiser = None
+                 ) -> None:
         self.weight_initialiser = weight_initialiser or getattr(activation, "weight_initialiser")
 
         self.weights = self.weight_initialiser(input_size, output_size)
         self.biases = np.zeros((output_size, 1))
         self.activation = activation
 
-        self.inputs = None
-        self.z = None
-        self.outputs = None
-        self.d_weights = None
-        self.d_biases = None
+        self.inputs: Optional[np.ndarray] = None
+        self.z: Optional[np.ndarray] = None
+        self.outputs: Optional[np.ndarray] = None
+        self.d_weights: Optional[np.ndarray] = None
+        self.d_biases: Optional[np.ndarray] = None
 
-    def forward(self, inputs):
+    def forward(self, inputs: np.ndarray) -> np.ndarray:
         self.inputs = inputs
         self.z = np.dot(self.weights, self.inputs) + self.biases
         self.outputs = self.activation.execute(self.z)
         return self.outputs
 
-    def backward(self, d_loss):
+    def backward(self, d_loss: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         d_z = d_loss * self.activation.derivative(self.z) # dL/dz = dL/da * da/dz
         self.d_weights = np.dot(d_z, self.inputs.T) # dL/dW = dL/dz * dz/dW
         self.d_biases = np.sum(d_z, axis=1, keepdims=True) # axis for rows instead
         d_inputs = np.dot(self.weights.T, d_z) # dL/dx = dL/dz * dz/dx
         return d_inputs, self.d_weights, self.d_biases
 
-    def update(self, optimiser):
+    def update(self, optimiser: Optimiser) -> None:
         optimiser.update(self.weights, self.biases,
                          self.d_weights, self.d_biases)

core/loss_functions.py
@@ -4,26 +4,26 @@ from abc import ABC, abstractmethod
 
 class Loss(ABC):
     @abstractmethod
-    def execute(self, y_pred, y_true):
+    def execute(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
         raise NotImplementedError
 
     @abstractmethod
-    def derivative(self, y_pred, y_true):
+    def derivative(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
 class MSELoss(Loss): # Regression
-    def execute(self, y_pred, y_true):
-        return np.mean((y_pred - y_true) ** 2)
+    def execute(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
+        return float(np.mean((y_pred - y_true) ** 2))
 
-    def derivative(self, y_pred, y_true):
+    def derivative(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
         n = np.prod(y_true.shape)
         return (2 / n) * (y_pred - y_true) # features * samples to get proper mean if you ever ask why division by n
 
 class CrossEntropyLoss(Loss): # Classification, y_true should be one-hot encoded
-    def execute(self, y_pred, y_true):
+    def execute(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
         epsilon = 1e-12
         y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
         return -np.sum(y_true * np.log(y_pred)) / y_true.shape[1]
 
-    def derivative(self, y_pred, y_true):
+    def derivative(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
         return (y_pred - y_true) / y_true.shape[1]

core/neural_network.py
@@ -1,19 +1,29 @@
 import numpy as np
+from typing import Optional, Tuple, List
 
+from core.activations import Activation
+from core.initialisers import Initialiser
 from core.layers import Layer
+from core.loss_functions import Loss
+from core.optimisers import Optimiser
+
 
 class NeuralNetwork:
-    def __init__(self, input_size):
+    def __init__(self, input_size: int) -> None:
         self.input_size = input_size
         self.layers = []
 
-    def add_layer(self, size, activation, weight_initialiser=None):
+    def add_layer(self,
+                  size: int,
+                  activation: Activation,
+                  weight_initialiser: Optional[Initialiser] = None
+                  ) -> None:
         input_size = self.input_size if not self.layers else self.layers[-1].weights.shape[0]
         self.layers.append(
             Layer(input_size, size, activation, weight_initialiser)
         )
 
-    def forward(self, x):
+    def forward(self, x: np.ndarray) -> Tuple[np.ndarray, List[np.ndarray]]:
         if x.shape[0] != self.input_size:
             x = x.reshape(self.input_size, -1)
 
@@ -24,7 +34,7 @@ class NeuralNetwork:
             activations.append(x)
         return x, activations
 
-    def predict(self, x):
+    def predict(self, x: np.ndarray) -> np.ndarray:
         if x.shape[0] != self.input_size:
             x = x.reshape(self.input_size, -1)
 
@@ -32,11 +42,11 @@ class NeuralNetwork:
             x = layer.forward(x)
         return x
 
-    def compute_loss(self, x, y_true, loss_fn):
+    def compute_loss(self, x: np.ndarray, y_true: np.ndarray, loss_fn: Loss) -> float:
         y_pred, _ = self.forward(x)
         return loss_fn.execute(y_pred, y_true)
 
-    def backpropagate(self, d_loss, optimiser):
+    def backpropagate(self, d_loss: np.ndarray, optimiser: Optimiser) -> np.ndarray:
         for layer in reversed(self.layers):
             d_loss, _, _ = layer.backward(d_loss) # derivatives
             layer.update(optimiser) # alter weights according to derivatives

core/optimisers.py
@@ -1,34 +1,45 @@
+from typing import Optional
+
 import numpy as np
 from abc import ABC, abstractmethod
 
 
 class Optimiser(ABC):
     @abstractmethod
-    def update(self, weights, biases, dW, db):
+    def update(self, weights: np.ndarray, biases: np.ndarray,
+               dW: np.ndarray, db: np.ndarray):
         raise NotImplementedError
 
 class SGD(Optimiser):
-    def __init__(self, learning_rate=1e-4):
+    def __init__(self, learning_rate: float = 1e-4):
         self.lr = learning_rate
 
-    def update(self, weights, biases, dW, db):
+    def update(self, weights: np.ndarray, biases: np.ndarray,
+               dW: np.ndarray, db: np.ndarray):
         weights -= self.lr * dW
         biases -= self.lr * db
 
 
 class SGDM(Optimiser):
-    def __init__(self, learning_rate=1e-4, gamma=0.9):
+    def __init__(self, learning_rate: float = 1e-4, gamma=0.9):
         self.lr = learning_rate
 
         self.gamma = gamma
-        self.v_w = None
-        self.v_b = None
+        self.v_w = {}
+        self.v_b = {}
 
-    def update(self, weights, biases, dW, db):
+    def update(self, weights: np.ndarray, biases: np.ndarray,
+               dW: np.ndarray, db: np.ndarray):
         """
         vt = γv_t−1 + η∇θJ(θ)
         θ = θ − vt
         """
+        # weights_id = id(weights)
+        # if weights_id not in self.v_w:
+        #     self.v_w[weights_id] = np.zeros_like(weights)
+        # ??? Optimiser does not work, weights and biases are not tracked per layer...
+
+
         if self.v_w is None or self.v_b is None:
             self.v_w = np.zeros_like(weights)
             self.v_b = np.zeros_like(biases)

learning/learner.py
@@ -1,13 +1,23 @@
 import numpy as np
 
+from core.neural_network import NeuralNetwork
+from core.loss_functions import Loss
+from core.optimisers import Optimiser
+
+
 class Learner:
-    def __init__(self, model, loss_fn, optimiser, batch_size=1):
+    def __init__(self,
+                 model: NeuralNetwork,
+                 loss_fn: Loss,
+                 optimiser: Optimiser,
+                 batch_size: int = 1
+                 ) -> None:
         self.model = model
         self.loss_fn = loss_fn
         self.optimiser = optimiser
         self.batch_size = batch_size
 
-    def learn_step(self, input_batch, output_batch):
+    def learn_step(self, input_batch: np.ndarray, output_batch: np.ndarray) -> float:
         pred, _ = self.model.forward(input_batch) # get model output
 
         loss = self.loss_fn.execute(pred, output_batch) # get loss
@@ -17,8 +27,12 @@ class Learner:
 
         return loss # other use cases
 
-    def learn(self, input_batch, output_batch, epochs,
-              verbose=True):
+    def learn(self,
+              input_batch: np.ndarray,
+              output_batch: np.ndarray,
+              epochs: int,
+              verbose: bool = True
+              ) -> "Learner":
         num_samples = input_batch.shape[1]
         for epoch in range(epochs):
             # shuffle and randomise the data to be used, helps with overfitting

main.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 from core.neural_network import NeuralNetwork
-from core.activations import LeakyReLU, Linear
+from core.activations import LeakyReLU, Linear, Tanh, ReLU
 from core.loss_functions import MSELoss
-from core.optimisers import SGD
+from core.optimisers import SGD, SGDM
 from learning.learner import Learner
 from core.initialisers import HeNormal, Normal
 
@@ -11,36 +11,40 @@ import matplotlib.pyplot as plt
 
 def main():
     np.random.seed(123)
-    
-    x_batch = np.random.uniform(-6, 6, (1, 1000))  # (1, 1000)
+
+    x_batch = np.random.uniform(-10, 10, (1, 1000))  # (1, 1000)
     y_batch = np.sin(x_batch)
 
-    learning_rate = 0.1
+    learning_rate = 0.005
 
     nn = NeuralNetwork(input_size=x_batch.shape[0])
-    nn.add_layer(64, LeakyReLU(alpha=0.01), weight_initialiser=HeNormal())
+    nn.add_layer(64, LeakyReLU(alpha=0.01), weight_initialiser=HeNormal()) # LeakyReLU(alpha=0.01)
     nn.add_layer(32, LeakyReLU(alpha=0.01), weight_initialiser=HeNormal())
     nn.add_layer(1, Linear())
+
+    x_mean, x_std = np.mean(x_batch), np.std(x_batch)
+    x_batch = (x_batch - x_mean) / x_std  # standard normal distribution z-score
 
     optimiser = SGD(learning_rate=learning_rate)
     learner = Learner(model=nn,
                       loss_fn=MSELoss(),
                       optimiser=optimiser,
-                      batch_size=128)
+                      batch_size=32)
 
-    epochs = 10000
+    epochs = 10001
 
     training_loss = learner.learn(x_batch, y_batch,
                                   epochs=epochs,
                                   verbose=True)
 
-    x_eval = np.linspace(-6, 6, 1000).reshape(1, -1)  # Shape: (1, 1000)
+    x_eval = np.linspace(-10, 10, 1000).reshape(1, -1)  # Shape: (1, 1000)
+    x_eval_normalized = (x_eval - x_mean) / x_std
     y_true = np.sin(x_eval)
-    y_pred = nn.forward(x_eval)
+    y_pred = nn.predict(x_eval_normalized)
 
     # Plotting
     plt.plot(x_eval.flatten(), y_true.flatten(), label="True sin(x)", color='blue')
-    plt.plot(x_eval.flatten(), y_pred[0].flatten(), label="NN Prediction", color='red')
+    plt.plot(x_eval.flatten(), y_pred.flatten(), label="NN Prediction", color='red')
     plt.legend()
     plt.title("Neural Network Approximation of sin(x)")
     plt.xlabel("x")